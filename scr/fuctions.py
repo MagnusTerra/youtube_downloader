34c0ce443e7444ea11892b60f00aa62c4faa14de
MagnusTerra <escalantejose22@gmail.com>
2024-05-25 15:27:43 -0600

    Add functions for downloading videos and playlists from YouTube

--- scr/fuctions.py
+++ scr/fuctions.py
@@ -35,3 +35,67 @@ def encode_video(video_in, video_out):
         print("Transcoding finished")
         os.remove(video_in)
         #os.rename("cosa.mp4", str(video_out) + ".mp4")
+        
+        
+def download_just_video(url):
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]',
+        'outtmpl': '%(title)s.%(ext)s',
+    }
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            
+    except Exception as e:
+        print(f"An error ocurred during download: {e}")
+        return
+    finally:
+        print("Download finished")
+        
+def download_video_audio(url):
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
+        'outtmpl': '%(title)s.%(ext)s',
+    }
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            
+    except Exception as e:
+        print(f"An error ocurred during download: {e}")
+        return
+    finally:
+        print("Download finished")
+        
+#Download a play list and create a dor with the name of the playlist
+def download_playlist(url, output_dir: str = None):
+    """
+    Downloads a YouTube playlist given its URL.
+
+    Args:
+        url (str): The URL of the YouTube playlist.
+        output_dir (str, optional): The directory where the downloaded files will be saved. If not provided, the files will be saved in the current directory.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If an error occurs during the download.
+
+    """
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
+        'outtmpl': '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s',
+    }
+    if output_dir is not None:
+        ydl_opts['outtmpl'] = output_dir + '/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'
+    
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            
+    except Exception as e:
+        print(f"An error occurred during download: {e}")
+        return
+    finally:
+        print("Download finished")
--- scr/test.py
+++ scr/test.py
@@ -1,3 +1,6 @@
-import os
+from fuctions import *
 
-print("The test will be executed here")
+if __name__ == "__main__":
+    youtube_url : str = "https://youtube.com/playlist?list=PL5CtC7ipPN7CbAEcfxHnfoco2nRxd6RtI&si=BIp4foNrJ9xOO259" 
+    
+    download_playlist(youtube_url,"E:")
